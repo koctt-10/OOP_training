f100c3d7c31a6aea775c60c749c393c67f389052
koctt <koctt10@gmail.com>
2024-08-23 20:11:38 +0800

    Add Ball class

---
+++ OOP/Edu/10.py
@@ -0,0 +1,27 @@
+class Ball():
+    MIN_COORD = 0
+    MAX_COORD = 100
+    
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+        
+    def set_coord(self, x, y):
+        if self.MIN_COORD <= x <= self.MAX_COORD:
+            self.x = x
+            self.y = y
+    
+    def __getattribute__(self, item) -> Any:
+        if item == "x":
+            raise ValueError("Er")
+        else:
+            return object.__getattribute__(self, item)
+        
+    def __setattr__(self, key, value) -> None:
+        if key == "z":
+            raise AttributeError("NONE")
+        else:
+            object.__setattr__(self, key, value)
+    
+    def __gelattr__(self, name: str) -> None:
+        pass
